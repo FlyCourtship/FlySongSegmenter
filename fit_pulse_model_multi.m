e2e0f5b102daa5df28eff76ffe22e088c0b7b1fe
David Stern <sternd@janelia.hhmi.org>
2012-02-07 12:01:20 -0500

    improved description

--- fit_pulse_model_multi.m
+++ fit_pulse_model_multi.m
@@ -1,6 +1,6 @@
 function fit_pulse_model_multi(folder)
 
-%function Process_multi_daq_Song(folder_containing_multiple_daqs,song_range)
+%function fit_pulse_model_multi(folder_containing_multiple_daqs,song_range)
 %This function allows you to analyze mutiple daqs in a folder and gives you
 %outputs in separate folders.
 
