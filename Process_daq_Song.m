da8d46c291fdd0333ceaf9eaabbaa95b0fb4dc7c
David Stern <sternd@janelia.hhmi.org>
2012-04-10 16:11:44 -0400

    Revert "Bug in Process_daq_Song didn't save pulseInfo2"
    This reverts commit 987eafdc6f7609d258353aacb09b6a2a710a5ae1.

--- Process_daq_Song.m
+++ Process_daq_Song.m
@@ -1,4 +1,4 @@
-function Process_daq_Song(song_daq_file,song_range)
+function Process_daq_Song(song_daq_file,channel_num,song_range,params_path)
 
 %old - when require noise file
 %function Process_daq_Song(song_daq_file,song_range)
@@ -7,15 +7,18 @@ function Process_daq_Song(song_daq_file,song_range)
 song_daqinfo = daqread(song_daq_file,'info');
 %noise_daqinfo = daqread(noise_daq_file,'info');
 
-if nargin == 1
-    range = 0;
-else
-    range=1;
-end
+%if nargin == 1
+%    range = 0;
+%else
+%    range=1;
+%end
 
 %Produce batch process file, with daq 
 
 nchannels_song = length(song_daqinfo.ObjInfo.Channel);
+if(~isempty(channel_num) && ((channel_num<1) || (channel_num>nchannels_song)))
+  warning('channel_num out of range');
+end
 %nchannels_noise = length(noise_daqinfo.ObjInfo.Channel);
 %grab sample rate from daq and replace value in params, with
 %warning       
@@ -29,14 +32,15 @@ new_dir = [pathstr sep name '_out'];
 mkdir(new_dir);
 
 
-for y = 1:nchannels_song
+if(isempty(channel_num))  yy=1:nchannels_song;  else  yy=channel_num;  end
+for y = yy
     outfile  = [new_dir sep 'PS_' name '_ch' num2str(y) '.mat'];
     file_exist = exist(outfile,'file');
     if file_exist == 0;%if file exists, skip
         %grab song and noise from each channel
         
         fprintf(['Grabbing song from daq file channel %s.\n'], num2str(y))
-        if range == 1
+        if ~isempty(song_range)
             song = daqread(song_daq_file,'Channels',y,'Samples',song_range);
         else
             song = daqread(song_daq_file,'Channels',y);
@@ -44,7 +48,7 @@ for y = 1:nchannels_song
  
         %run Process_Song on selected channel
         fprintf('Processing song.\n')
-        [data, winnowed_sine, pulseInfo, pulseInfo2, pps, pcndInfo] = Process_Song(song);
+        [data, winnowed_sine, pcndInfo, pulseInfo, pulseInfo2] = Process_Song(song,[],params_path);
         %save data
         
         save(outfile, 'data','winnowed_sine', 'pcndInfo','pulseInfo2','pulseInfo','-v7.3')
