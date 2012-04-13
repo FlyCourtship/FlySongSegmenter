131a3be91bafaa7a78e34a1585b851d9c2a6aa66
Ben Arthur <arthurb@janelia.hhmi.org>
2012-04-13 12:23:25 -0400

    cluster.sh now actually works

--- cluster.sh
+++ cluster.sh
@@ -3,8 +3,8 @@
 #cd to folder containing cluster.sh and then execute:
 #./cluster.sh  full_path_to_folder_of_.daqs  full_path_to_params.m  number_of_channels
 
-#each channel of each file is sent to it's own whole node.  all files assumed to have the
-#same number of channels.
+#each channel of each file is sent to it's own whole node.  all files
+#assumed to have the same number of channels.
 
 daq_folder="$1"
 params_path="$2"
@@ -21,6 +21,6 @@ do
   clean_daq_name=$(echo "$daq_name" | sed "s/[^a-zA-Z0-9 ]/_/g")
   for i in $(seq $nchan)
   do
-    qsub -N "FSS-$clean_daq_name-$i-$clean_params_name" -pe batch 8 -l short=true -b y -j y -cwd -o "$daq_folder/$daq_file/$daq_name-$i-$params_name.log" -V ./find_fly_song/distrib/run_find_fly_song.sh /usr/local/matlab-2012a "\"$daq_file\"" -p "\"$params_path\"" -c "$i" -s 1:1000000
+    qsub -N "FSS-$clean_daq_name-$i-$clean_params_name" -pe batch 4 -l mem96=true -b y -j y -cwd -o "$clean_daq_name-$i-$clean_params_name.log" -V ./find_fly_song/distrib/run_find_fly_song.sh /usr/local/matlab-2012a "\"$daq_file\"" -p "\"$params_path\"" -c "$i"
   done
 done
