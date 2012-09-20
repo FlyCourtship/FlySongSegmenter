6018abe31a4d0e7a8edbeae349494654a5f48350
Ben Arthur <arthurb@janelia.hhmi.org>
2012-09-20 12:30:19 -0400

    cluster.sh now accepts a daq file, in addition to a folder of daqs

--- cluster.sh
+++ cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #cd to folder containing cluster.sh and then execute:
-#./cluster.sh  full_path_to_folder_of_.daqs  full_path_to_params.m  number_of_channels
+#./cluster.sh  full_path_to_file_or_folder_of_.daqs  full_path_to_params.m  number_of_channels
 
 #each channel of each file is sent to it's own half node.  all files
 #assumed to have the same number of channels.
 
-daq_folder="$1"
+daq_file_or_folder="$1"
 params_path="$2"
 nchan="$3"
 params_name=$(basename "$params_path" ".m")
@@ -14,7 +14,15 @@ clean_params_name=$(echo "$params_name" | sed "s/[^a-zA-Z0-9 ]/_/g")
 
 IFS=$'\n'
 
-for daq_file in $(ls $daq_folder/*.daq)
+if [ -d $daq_file_or_folder ] ; then
+  daq_files=$(ls -1 $daq_file_or_folder/*.daq)
+  daq_folder=$daq_file_or_folder
+else
+  daq_files=$daq_file_or_folder
+  daq_folder=$(dirname $daq_file_or_folder)
+fi
+for daq_file in $daq_files
+#for daq_file in $(ls $daq_folder/*.daq)
 do
   unset IFS
   daq_name=$(basename "$daq_file" ".daq")
