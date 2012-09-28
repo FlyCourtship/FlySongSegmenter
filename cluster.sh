e32fc744da8109945037c30a6bd43e49bb20f090
Ben Arthur <arthurb@janelia.hhmi.org>
2012-09-28 17:12:32 -0400

    removed short=true in cluster.sh as w/o parfor it takes awhile

--- cluster.sh
+++ cluster.sh
@@ -33,7 +33,7 @@ do
   for i in $(seq $nchan)
   do
     #qsub -N FSS-$clean_daq_name-$i-$clean_params_name -l short=true -pe batch 4 -b y -j y -cwd -o $daq_folder/$daq_name\_out/$clean_daq_name-$i-$clean_params_name.log -V ./cluster2.sh "\"$daq_file\"" -p "\"$params_path\"" -c "$i"
-    qsub -N FSS-$clean_daq_name-$i-$clean_params_name -l short=true -b y -j y -cwd -o $daq_folder/$daq_name\_out/$clean_daq_name-$i-$clean_params_name.log -V ./cluster2.sh "\"$daq_file\"" -p "\"$params_path\"" -c "$i"
+    qsub -N FSS-$clean_daq_name-$i-$clean_params_name -b y -j y -cwd -o $daq_folder/$daq_name\_out/$clean_daq_name-$i-$clean_params_name.log -V ./cluster2.sh "\"$daq_file\"" -p "\"$params_path\"" -c "$i"
     #sleep 1
   done
 done
