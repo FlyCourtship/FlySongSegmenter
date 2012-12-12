6737a76bc52e848d15b42848cff50d8496dc55ff
Ben Arthur <arthurb@janelia.hhmi.org>
2012-12-12 16:53:28 -0500

    GetClips now works with Data.d too

--- GetClips.m
+++ GetClips.m
@@ -1,13 +1,23 @@
-function clips=GetClips(file,channel,start_times,stop_times)
+function clips = GetClips(start_times, stop_times, varargin)
+%function clips = GetClips(start_times, stop_times, file, channel)
+%function clips = GetClips(start_times, stop_times, data)
+%
+%e.g.
+%Pulses.IPICull.x = GetClips(Pulses.IPICull.w0, Pulses.IPICull.w1, '../data/augusto.daq', 3);
+%Sines.LengthCull.clips = GetClips(Sines.LengthCull.start, Sines.LengthCull.stop, Data.d);
 
-%Pulses.IPICull.clips=GetClips('../data/augusto.daq',3,Pulses.IPICull.w0,Pulses.IPICull.w1);
-%Sines.LengthCull.clips=GetClips('../data/augusto.daq',3,Sines.LengthCull.start,Sines.LengthCull.stop);
-
-if(~strcmp(file(end-3:end),'.daq'))
-  error('only .daq files supported currently');
+if(nargin==4)
+  file=varargin{1};
+  channel=varargin{2};
+  if(~strcmp(file(end-3:end),'.daq'))
+    error('only .daq files supported currently');
+  end
+  data = daqread(file,'Channels',channel);
+elseif(nargin==3)
+  data = varargin{1};
+else
+  error('invalid input args');
 end
-
-data = daqread(file,'Channels',channel);
 
 clips=cell(1,length(start_times));
 for c=1:length(start_times)
