2ee60dbbaf04fbba33d706afab6a941672ee3630
Ben Arthur <arthurb@janelia.hhmi.org>
2012-12-20 13:24:55 -0500

    converted PlotMultiTaperFTest to new data structure scheme

--- PlotMultiTaperFTest.m
+++ PlotMultiTaperFTest.m
@@ -1,64 +1,107 @@
-%function PlotMultiTaperFTest(ssf,freq_range)
+%function PlotMultiTaperFTest(sines,params,data,freq_range)
 %
 %utility to plot the output of MultiTaperFTest.  usage:
-%PlotMultiTaperFTest(Sine.MultiTaper,[50 250]);
+%PlotMultiTaperFTest(Sines.MultiTaper,Params,Data,[50 250]);
 
-function PlotMultiTaperFTest(ssf,freq_range)
+function PlotMultiTaperFTest(sines,params,data,freq_range)
 
-global SSF
+handles.sines=sines;
+handles.params=params;
+handles.data=data;
+handles.freq_range=freq_range;
 
-SSF=ssf;
-%SSF.t = SSF.t ./ SSF.fs;
+handles.figure1=figure;
+foo=get(handles.figure1,'Position');
+set(handles.figure1,'Position',round([400 250 1.5*foo(3) 1.5*foo(4)]));
+tmp=get(gcf,'position');  tmp2=get(gcf,'color');
 
-if(~isfield(SSF,'h'))
-  SSF.h=figure;
-  foo=get(SSF.h,'Position');
-  set(SSF.h,'Position',round([400 250 1.5*foo(3) 1.5*foo(4)]));
-  tmp=get(gcf,'position');  tmp2=get(gcf,'color');
+uicontrol(handles.figure1,'style','pushbutton','string','zoom in', ...
+   'callback', @zoom_in_callback, ...
+   'position',[0 0 70 20]);
+uicontrol(handles.figure1,'style','pushbutton','string','zoom out', ...
+   'callback', @zoom_out_callback, ...
+   'position',[75 0 70 20]);
+uicontrol(handles.figure1,'style','pushbutton','string','pan left', ...
+   'callback', @pan_left_callback, ...
+   'position',[150 0 70 20]);
+uicontrol(handles.figure1,'style','pushbutton','string','pan right', ...
+   'callback', @pan_right_callback, ...
+   'position',[225 0 70 20]);
 
-  zoom_in_button=uicontrol('style','pushbutton','string','zoom in', ...
-     'callback', ['global SSF;  tmp1=(SSF.left+SSF.right)/2;  tmp2=SSF.right-SSF.left;  SSF.left=max(0,tmp1-tmp2/4);  SSF.right=min((length(SSF.d)-1)/SSF.fs,tmp1+tmp2/4);  PlotMultiTaperFTest(SSF,[' num2str(freq_range) ']);'], ...
-     'position',[0 0 70 20]);
-  zoom_out_button=uicontrol('style','pushbutton','string','zoom out', ...
-     'callback', ['global SSF;  tmp1=(SSF.left+SSF.right)/2;  tmp2=SSF.right-SSF.left;  SSF.left=max(0,tmp1-tmp2);  SSF.right=min((length(SSF.d)-1)/SSF.fs,tmp1+tmp2);  PlotMultiTaperFTest(SSF,[' num2str(freq_range) ']);'], ...
-     'position',[75 0 70 20]);
-  pan_left_button=uicontrol('style','pushbutton','string','pan left', ...
-     'callback', ['global SSF;  tmp2=min(SSF.left,(SSF.right-SSF.left)/2);  SSF.left=SSF.left-tmp2;  SSF.right=SSF.right-tmp2;  PlotMultiTaperFTest(SSF,[' num2str(freq_range) ']);'], ...
-     'position',[150 0 70 20]);
-  pan_right_button=uicontrol('style','pushbutton','string','pan right', ...
-     'callback', ['global SSF;  tmp2=min((length(SSF.d)-1)/SSF.fs-SSF.right,(SSF.right-SSF.left)/2);  SSF.left=SSF.left+tmp2;  SSF.right=SSF.right+tmp2;  PlotMultiTaperFTest(SSF,[' num2str(freq_range) ']);'], ...
-     'position',[225 0 70 20]);
+handles.left=0;
+handles.right=(length(handles.data.d)-1)/handles.data.fs;
 
-  SSF.left=0;
-  SSF.right=(length(SSF.d)-1)/SSF.fs;
-else
-  figure(SSF.h);
-end
+plot_it(handles);
 
-%[SSF.left SSF.right]
+guidata(handles.figure1,handles);
+
+
+function plot_it(handles)
 
 ax1=subplot(5,1,1);
-idx=1+(round(SSF.left*SSF.fs):round(SSF.right*SSF.fs));
-plot(idx./SSF.fs,SSF.d(idx),'k-');
-title(['NW=' num2str(SSF.NW) ', K=' num2str(SSF.K) ', dT=' num2str(SSF.dT) ', dS=' num2str(SSF.dS) ', pval=' num2str(SSF.pval)]);
+idx=1+(round(handles.left*handles.data.fs):round(handles.right*handles.data.fs));
+plot(idx./handles.data.fs,handles.data.d(idx),'k-');
+title(['NW=' num2str(handles.params.NW) ', K=' num2str(handles.params.K) ', dT=' num2str(handles.params.dT) ', dS=' num2str(handles.params.dS) ', pval=' num2str(handles.params.pval)]);
 axis tight
 
 ax2=subplot(5,1,[2 3]);
-tmpT=SSF.t/SSF.fs+SSF.dT/2-SSF.dS/2;
-idxT=find(((tmpT+SSF.dS)>=SSF.left) & ((tmpT-SSF.dS)<=SSF.right));
-idxF=find((SSF.f>=freq_range(1)) & (SSF.f<=freq_range(2)));
-surf(tmpT(idxT),SSF.f(idxF),double(abs(SSF.A(idxF,idxT))),'EdgeColor','none');
-%surf(tmpT(idxT),SSF.f-min(diff(SSF.f))/2,log10(SSF.Fval(:,idxT)),'EdgeColor','none');
+tmpT=handles.sines.t/handles.data.fs+handles.params.dT/2-handles.params.dS/2;
+idxT=find(((tmpT+handles.params.dS)>=handles.left) & ((tmpT-handles.params.dS)<=handles.right));
+idxF=find((handles.sines.f>=handles.freq_range(1)) & (handles.sines.f<=handles.freq_range(2)));
+surf(tmpT(idxT),handles.sines.f(idxF),double(abs(handles.sines.A(idxF,idxT))),'EdgeColor','none');
+%surf(tmpT(idxT),handles.f-min(diff(handles.f))/2,log10(handles.Fval(:,idxT)),'EdgeColor','none');
 axis xy;  axis tight;  view(0,90);
-axis([SSF.left SSF.right freq_range]);
+axis([handles.left handles.right handles.freq_range]);
 colormap(flipud(gray));
 ylabel('frequency (Hz)');
 
 ax3=subplot(5,1,[4 5]);
-idx=find((SSF.events(:,1)>=SSF.left*SSF.fs) & (SSF.events(:,1)<=SSF.right*SSF.fs));
-plot(SSF.events(idx,1)./SSF.fs,SSF.events(idx,2),'k.','markersize',24);
-axis([SSF.left SSF.right freq_range]);
+idx=find((handles.sines.events(:,1)>=handles.left*handles.data.fs) & (handles.sines.events(:,1)<=handles.right*handles.data.fs));
+plot(handles.sines.events(idx,1)./handles.data.fs,handles.sines.events(idx,2),'k.','markersize',24);
+axis([handles.left handles.right handles.freq_range]);
 grid on;
 xlabel('time (s)');
 ylabel('frequency (Hz)');
 linkaxes([ax1 ax2 ax3],'x');
+
+
+function zoom_in_callback(obj,evt)
+
+handles=guidata(obj);
+tmp1=(handles.left+handles.right)/2;
+tmp2=handles.right-handles.left;
+handles.left=max(0,tmp1-tmp2/4);
+handles.right=min((length(handles.data.d)-1)/handles.data.fs,tmp1+tmp2/4);
+plot_it(handles);
+guidata(handles.figure1,handles);
+
+
+function zoom_out_callback(obj,evt)
+
+handles=guidata(obj);
+tmp1=(handles.left+handles.right)/2;
+tmp2=handles.right-handles.left;
+handles.left=max(0,tmp1-tmp2);
+handles.right=min((length(handles.data.d)-1)/handles.data.fs,tmp1+tmp2);
+plot_it(handles);
+guidata(handles.figure1,handles);
+
+
+function pan_left_callback(obj,evt)
+
+handles=guidata(obj);
+tmp2=min(handles.left,(handles.right-handles.left)/2);
+handles.left=handles.left-tmp2;
+handles.right=handles.right-tmp2;
+plot_it(handles);
+guidata(handles.figure1,handles);
+
+
+function pan_right_callback(obj,evt)
+
+handles=guidata(obj);
+tmp2=min((length(handles.data.d)-1)/handles.data.fs-handles.right,(handles.right-handles.left)/2);
+handles.left=handles.left+tmp2;
+handles.right=handles.right+tmp2;
+plot_it(handles);
+guidata(handles.figure1,handles);
