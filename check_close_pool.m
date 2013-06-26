2fedad8914a4a0f7a3f56cab6fac04e41832138c
Ben Arthur <arthurb@janelia.hhmi.org>
2013-06-26 09:45:15 -0400

    matlabpool close error now caught

--- check_close_pool.m
+++ check_close_pool.m
@@ -4,6 +4,10 @@ function check_close_pool(poolavail,isOpen)
 
 if isOpen == -1%if pool opened in this script, then close
   if poolavail~=0
-    matlabpool close
+    try
+      matlabpool close
+    catch
+      disp('WARNING:  problem closing matlabpool.  will save results and finish anyway');
+    end
   end
 end
